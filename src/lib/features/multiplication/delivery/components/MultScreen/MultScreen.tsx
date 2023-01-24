eb3c0c8f4515f6e09717f46c89f3b7024fe81686
Sam Komarov <sam@skomarov.com>
2023-01-24 11:46:11 +0300

    allow only numeric input in the answer field

--- src/lib/features/multiplication/delivery/components/MultScreen/MultScreen.tsx
+++ src/lib/features/multiplication/delivery/components/MultScreen/MultScreen.tsx
@@ -1,5 +1,6 @@
 import "./MultScreen.css";
 import MultState from "../../../domain/state/MultState";
+import { useState } from "react";
 
 export function MultScreen({state} : {state: MultState}) {
     return (
@@ -14,7 +15,17 @@ export function MultScreen({state} : {state: MultState}) {
 }
 
 function AnswerInput() {
-    return <input id="answer"></input>;
+    const [text, setText] = useState("");
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setText(event.target.value.replace(/[^0-9]/g, '')) 
+    };
+    return (
+        <input 
+            id="answer"
+            value={text} 
+            onChange={handleChange}
+        />
+    );
 }
 
 export default MultScreen;
